38bd285b289ef0a426b43ae0b9967e04c65736b7
xfjin <xfjin@iflytek.com>
2021-01-27 17:41:54 +0800

    20200127

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -4,7 +4,27 @@ import 'app_list.dart';
 import 'app_random_words.dart';
 
 void main() {
-  runApp(MyApp());
+
+  // runApp(MyApp());
+  runApp(MaterialApp(
+    title: "demo",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("demo"),
+      ),
+      body: Text("hello"),
+    ),
+  ));
 }
+
+Iterable<String> getEmoji(int count) sync* {
+  const oneSecond = Duration(seconds: 1);
+  Runes first = Runes('\u{1f47f}');
+  for (int i = 0; i < count; i++) {
+    Future.delayed(oneSecond);
+    yield String.fromCharCodes(first.map((e) => e + i));
+  }
+}
+
 
 
